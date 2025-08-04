038f9b1832c7312ea024be210aa70795d439589b
Deepak Sharma <deepaksharma1982@live.in>
2025-08-04 11:52:33 +1000

    fix the favicon path

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="../public/favicon.ico" />
       </Head>
       <body id="bg-img">
         <Main />
